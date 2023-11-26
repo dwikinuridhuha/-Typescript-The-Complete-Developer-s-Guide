0cf6bac17bd5f4665b3a4e3fec56e018e793cffe
dwiki <dwiki.nuridhuha@gmail.com>
2023-11-27 03:19:34 +0700

    video 110

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import fs from "fs";
+import {resolveTxt} from "dns";
 
-const matches = fs.readFileSync("footboll.csv", {encoding: "utf-8"})
+const matches = fs.readFileSync("football.csv", {encoding: "utf-8"})
     .split("/n")
-    .map((row) => {
+    .map((row): string[] => {
         return row.split(",");
     });
 
-console.log(matches);
+enum MatchResult {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D"
+}
+
+let manUnitedWins = 0
+
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] == "H") {
+        manUnitedWins++;
+    } else if (match[2] === "Man United" && match[5] == "A") {
+        manUnitedWins++;
+    }
+}
+
+console.log(manUnitedWins);
